78305b1645f60dc46e69dd3dd4ddda646e4695e3
Peisen Sun <sps@snnu.edu.cn>
2023-04-23 22:03:54 +0800

    xxx

--- Algorithm/graph.py
+++ Algorithm/graph.py
@@ -1,11 +1,18 @@
 import networkx as nx
+from distribution import *
 
 
 def build_graph(gmm_dict):
     gmm_dict: dict
     # build graph
     graph = nx.Graph()
-    for gene_id in gmm_dict:
+    gene_list = list(gmm_dict.keys())
+    gene_counts = len(gene_list)
+    for gene_id in gene_list:
         graph.add_node(gene_id)
     # calculate the weight
-
+    for i in range(gene_counts):
+        for j in range(gene_counts):
+            if i != j and not graph.has_edge(gene_list[i], gene_list[j]):
+                distance = distribution_distance(gmm_dict[gene_list[i]], gmm_dict[gene_list[j]])
+                graph.add_edge(gene_list[i], gene_list[j], weight=distance)
