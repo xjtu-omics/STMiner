68c6ba1f0f1cc727ccb587bb4612bb2d7b03d4a7
Peisen Sun <sps@snnu.edu.cn>
2023-04-23 22:14:02 +0800

    Update graph.py

--- Algorithm/graph.py
+++ Algorithm/graph.py
@@ -8,11 +8,13 @@ def build_graph(gmm_dict):
     graph = nx.Graph()
     gene_list = list(gmm_dict.keys())
     gene_counts = len(gene_list)
+    # add nodes
     for gene_id in gene_list:
         graph.add_node(gene_id)
-    # calculate the weight
+    # calculate the weight and add edges
     for i in range(gene_counts):
         for j in range(gene_counts):
             if i != j and not graph.has_edge(gene_list[i], gene_list[j]):
                 distance = distribution_distance(gmm_dict[gene_list[i]], gmm_dict[gene_list[j]])
                 graph.add_edge(gene_list[i], gene_list[j], weight=distance)
+    return graph
