4694995a9d45035c5a99055257bf32005864307d
Peisen Sun <sps@snnu.edu.cn>
2023-04-23 08:54:14 +0800

    Update graph.py

--- Algorithm/graph.py
+++ Algorithm/graph.py
@@ -1,2 +1,11 @@
 import networkx as nx
 
+
+def build_graph(gmm_dict):
+    gmm_dict: dict
+    # build graph
+    graph = nx.Graph()
+    for gene_id in gmm_dict:
+        graph.add_node(gene_id)
+    # calculate the weight
+
