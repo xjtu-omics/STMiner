2005c77cb07e16cc5cd2134e79005d940e9b0178
TypeError <sps@snnu.edu.cn>
2023-04-28 16:18:48 +0800

    xxx

--- Algorithm/Algorithm.py
+++ Algorithm/Algorithm.py
@@ -3,8 +3,7 @@ import numpy as np
 from scipy.optimize import linear_sum_assignment
 
 
-def linear_sum(cost):
-    cost: np.array
+def linear_sum(cost: np.array):
     row_ind, col_ind = linear_sum_assignment(cost)
     min_cost = cost[row_ind, col_ind].sum()
     return min_cost
