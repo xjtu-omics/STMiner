f852e594b33dc418d17c7b550a070d61f9bad54d
Peisen Sun <sps@snnu.edu.cn>
2023-06-08 10:29:05 +0800

    Update distribution.py

--- Algorithm/distribution.py
+++ Algorithm/distribution.py
@@ -94,15 +94,59 @@ def get_hellinger_distance(gmm1_covs, gmm1_means, gmm2_covs, gmm2_means):
     return hellinger_distance
 
 
+def fit_gmm_bic(adata: anndata,
+                gene_name: str,
+                min_n_comp: int = 2,
+                max_n_comp: int = 10,
+                max_iter: int = 200):
+    """
+    Representation of a Gaussian mixture model probability distribution.
+    Estimate the parameters of a Gaussian mixture distribution.
+
+    Estimate model parameters with the EM algorithm.
+    :param max_n_comp:
+    :type max_n_comp:
+    :param adata: Anndata of spatial data
+    :type adata: Anndata
+    :param gene_name: The gene name to fit
+    :type gene_name: str
+    :param min_n_comp: The min number of mixture components.
+    :type min_n_comp: int
+    :param max_iter: The number of EM iterations to perform.
+    :type max_iter: int
+    :return: The number of components and the fitted mixture.
+    :rtype: GaussianMixture
+    """
+    dense_array = get_exp_array(adata, gene_name)
+    result = np.array(array_to_list(dense_array))
+    # Number of unique center must be larger than the number of components.
+    if len(set(map(tuple, result))) > min_n_comp:
+        bic_list = []
+        gmm_list = []
+        for i in range(min_n_comp, max_n_comp):
+            gmm = mixture.GaussianMixture(n_components=i, max_iter=max_iter)
+            gmm.fit(result)
+            bic = gmm.bic(result)
+            bic_list.append(bic)
+            gmm_list.append(gmm)
+            # print(str(i) + ": " + str(bic))
+        index = bic_list.index(min(bic_list))
+        n_comp = min_n_comp + index
+        return n_comp, gmm_list[index]
+    else:
+        return None
+
+
 def fit_gmm(adata: anndata,
             gene_name: str,
-            n_comp: int = 10,
+            n_comp: int = 2,
             max_iter: int = 200):
     """
     Representation of a Gaussian mixture model probability distribution.
     Estimate the parameters of a Gaussian mixture distribution.
 
     Estimate model parameters with the EM algorithm.
+
     :param adata: Anndata of spatial data
     :type adata: Anndata
     :param gene_name: The gene name to fit
@@ -111,11 +155,11 @@ def fit_gmm(adata: anndata,
     :type n_comp: int
     :param max_iter: The number of EM iterations to perform.
     :type max_iter: int
-    :return: The fitted mixture.
+    :return: The number of components and the fitted mixture.
     :rtype: GaussianMixture
     """
     dense_array = get_exp_array(adata, gene_name)
-    result = array_to_list(dense_array)
+    result = np.array(array_to_list(dense_array))
     # Number of unique center must be larger than the number of components.
     if len(set(map(tuple, result))) > n_comp:
         gmm = mixture.GaussianMixture(n_components=n_comp, max_iter=max_iter)
@@ -141,7 +185,7 @@ def fit_gmms(adata,
              n_comp: int = 5,
              max_iter: int = 100):
     """
-    Same as fit_gmm, accepts a list of gene name.
+    Same as fit_gmm_bic, accepts a list of gene name.
     Representation of a Gaussian mixture model probability distribution.
     Estimate the parameters of a Gaussian mixture distribution.
     :param adata: Anndata of spatial data
@@ -160,7 +204,7 @@ def fit_gmms(adata,
     dropped_genes_count = 0
     for gene_id in tqdm(gene_name_list, desc='Processing ...'):
         try:
-            fit_result = fit_gmm(adata, gene_id, n_comp=n_comp, max_iter=max_iter)
+            _, fit_result = fit_gmm_bic(adata, gene_id, n_comp=n_comp, max_iter=max_iter)
             if fit_result is not None:
                 gmm_dict[gene_id] = fit_result
             else:
@@ -168,8 +212,53 @@ def fit_gmms(adata,
         except Exception as e:
             error_msg = str(e)
             raise ValueError("Gene id: " + gene_id + "\nError: " + error_msg)
-    print('Dropped genes number:' + str(dropped_genes_count))
+    if dropped_genes_count > 0:
+        print('Number of dropped genes: ' + str(dropped_genes_count))
     return gmm_dict
+
+
+def fit_gmms_bic(adata,
+                 gene_name_list: list,
+                 min_n_comp: int = 5,
+                 max_n_comp: int = 10,
+                 max_iter: int = 100):
+    """
+    Same as fit_gmm_bic, accepts a list of gene name.
+    Representation of a Gaussian mixture model probability distribution.
+    Estimate the parameters of a Gaussian mixture distribution.
+    :param max_n_comp:
+    :type max_n_comp: int
+    :param min_n_comp:
+    :type min_n_comp: int
+    :param adata: Anndata of spatial data
+    :type adata: Anndata
+    :param gene_name_list: The python list, each element is a gene name in adata.
+    :type gene_name_list: list
+    :param max_iter: The number of EM iterations to perform.
+    :type max_iter: int
+    :return: A Python dict of given genes list, key is gene name, value is GMM object.
+    :return: gmm_dict and comp_dict
+    :rtype: dict
+    """
+    gmm_dict = {}
+    comp_dict = {}
+    for gene_id in tqdm(gene_name_list, desc='Processing ...'):
+        try:
+            n_comp, fit_result = fit_gmm_bic(adata,
+                                             gene_id,
+                                             min_n_comp=min_n_comp,
+                                             max_n_comp=max_n_comp,
+                                             max_iter=max_iter)
+            gmm_dict[gene_id] = fit_result
+            if n_comp not in comp_dict.keys():
+                comp_dict[n_comp] = []
+                comp_dict[n_comp].append(gene_id)
+            else:
+                comp_dict[n_comp].append(gene_id)
+        except Exception as e:
+            error_msg = str(e)
+            raise ValueError("Gene id: " + gene_id + "\nError: " + error_msg)
+    return gmm_dict, comp_dict
 
 
 def fit_gmms_multiprocessing(adata: anndata,
@@ -178,7 +267,7 @@ def fit_gmms_multiprocessing(adata: anndata,
                              max_iter: int = 1000,
                              thread: int = 4) -> dict:
     """
-    Same as fit_gmm, use multiple threads.
+    Same as fit_gmm_bic, use multiple threads.
     Representation of a Gaussian mixture model probability distribution.
     Estimate the parameters of a Gaussian mixture distribution.
 
@@ -208,7 +297,7 @@ def fit_gmms_multiprocessing(adata: anndata,
 
 
 def _fit_worker(shared_dict, adata, gene_name, n_comp, max_iter):
-    shared_dict[gene_name] = fit_gmm(adata, gene_name, n_comp, max_iter)
+    shared_dict[gene_name] = fit_gmm_bic(adata, gene_name, n_comp, max_iter)
 
 
 def view_gmm(gmm, scope, bin_count=None):
