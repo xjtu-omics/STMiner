666fa1d2596cd577caa6bfaa7b3adaef6fa70f9e
Peisen Sun <sps@snnu.edu.cn>
2023-08-07 20:16:48 +0800

    Update test_noise.py

--- test/test_noise.py
+++ test/test_noise.py
@@ -1,19 +1,46 @@
-# %%
 import pandas as pd
+import numpy as np
 from anndata import AnnData
 
 from test.SPFinderTester import SPFinderTester
 
-spft = SPFinderTester()
 count = pd.read_csv('./data/test2/simulate_exp.csv', sep=',', index_col=0)
 position = pd.read_csv('./data/test2/simulate_position.csv', sep=',', index_col=0)
 adata = AnnData(X=count)
 adata.obs = position.reindex(adata.obs.index)
-spft.set_adata(adata)
-spft.normalize()
-spft.fit_pattern(n_top_genes=200, n_comp=20)
-spft.build_distance_array()
-spft.cluster_gene(n_clusters=3, mds_components=30)
-# %%
-label_df = spft.genes_labels
-spft.plot_pattern()
+
+for interval in [20, 10, 5, 4, 3, 2]:
+    spft = SPFinderTester()
+    spft.set_adata(adata)
+    spft.set_noise_type('periodicity', interval)
+    spft.normalize()
+    spft.fit_pattern(n_top_genes=300, n_comp=20)
+    spft.build_distance_array()
+    spft.cluster_gene(n_clusters=3, mds_components=30)
+    result = spft.genes_labels
+    name = 'periodicity_' + str(interval) + '.csv'
+    result.to_csv(name, sep=',')
+
+for mean in np.arange(0.1, 2, 0.2, dtype=np.float32):
+    spft = SPFinderTester()
+    spft.set_adata(adata)
+    spft.set_noise_type('gauss', mean)
+    spft.normalize()
+    spft.fit_pattern(n_top_genes=300, n_comp=20)
+    spft.build_distance_array()
+    spft.cluster_gene(n_clusters=3, mds_components=30)
+    result = spft.genes_labels
+    name = 'gauss_' + str(mean) + '.csv'
+    result.to_csv(name, sep=',')
+
+for percentage in np.arange(0.1, 1, 0.1, dtype=np.float32):
+    spft = SPFinderTester()
+    spft.set_adata(adata)
+    spft.set_noise_type('sp', percentage)
+    spft.normalize()
+    spft.fit_pattern(n_top_genes=300, n_comp=20)
+    spft.build_distance_array()
+    spft.cluster_gene(n_clusters=3, mds_components=30)
+    result = spft.genes_labels
+    name = 'sp_' + str(percentage) + '.csv'
+    result.to_csv(name, sep=',')
