baa6bf14882ad320ef547ebf1e988b9439b82afc
Peisen Sun <sps@snnu.edu.cn>
2023-09-12 15:13:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='SCGF',
+    name='STMiner',
     version='1.0',
+    author='PSSUN',
+    author_email='sunpeisen@stu.xjtu.edu.cn',
     packages=find_packages(),
-    install_requires=[]
 )
