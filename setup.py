071134ee0a8ae84c3935a7a3f385f0654f72e3a6
Peisen Sun <sps@snnu.edu.cn>
2023-09-12 15:21:26 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='STMiner',
-    version='1.0',
+    version='1.0.0',
     author='PSSUN',
+    license='MIT License',
+    description='Spatial transcriptomics data analysis',
     author_email='sunpeisen@stu.xjtu.edu.cn',
     packages=find_packages(),
+    keywords=['STMiner',
+              'gaussian mixture models',
+              'GMM',
+              'hellinger distance',
+              'Spatial transcriptomics']
 )
