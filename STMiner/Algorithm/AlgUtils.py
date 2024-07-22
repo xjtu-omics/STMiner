104ed726ecf88cc1d5c31fa6cf94d07620bbd2b1
Peisen Sun <sps@snnu.edu.cn>
2024-07-22 15:10:47 +0800

    Update AlgUtils.py

--- STMiner/Algorithm/AlgUtils.py
+++ STMiner/Algorithm/AlgUtils.py
@@ -3,14 +3,23 @@ from scipy import sparse
 
 
 def get_exp_array(adata, gene_name, remove_low_exp_spots=False):
+    sparse_matrix = _preprocess(adata, gene_name)
+    return _to_dense(remove_low_exp_spots, sparse_matrix)
+
+
+def _to_dense(remove_low_exp_spots, sparse_matrix):
+    dense_array = sparse_matrix.todense()
+    if remove_low_exp_spots:
+        dense_array = np.maximum(dense_array - np.mean(dense_array[dense_array != 0]), 0)
+    dense_array = np.array(np.round(dense_array), dtype=np.int32)
+    return dense_array
+
+
+def _preprocess(adata, gene_name):
     exp_array = adata[:, adata.var_names == gene_name].X
     if sparse.issparse(exp_array):
         data = np.array(exp_array.todense())
     else:
         data = np.array(exp_array)
     sparse_matrix = sparse.coo_matrix((data[:, 0], (np.array(adata.obs['x']), np.array(adata.obs['y']))))
-    dense_array = sparse_matrix.todense()
-    if remove_low_exp_spots:
-        dense_array = np.maximum(dense_array - np.mean(dense_array[dense_array != 0]), 0)
-    dense_array = np.array(np.round(dense_array), dtype=np.int32)
-    return dense_array
+    return sparse_matrix
