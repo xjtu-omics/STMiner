ec95742d00ff9cdabdb27a60dba6cbbc47fa5818
Peisen Sun <sps@snnu.edu.cn>
2024-09-02 22:10:31 +0800

    Update read_h5ad.py

--- STMiner/IO/read_h5ad.py
+++ STMiner/IO/read_h5ad.py
@@ -17,14 +17,14 @@ def read_h5ad(file, amplification=1, bin_size=1):
     """
     amplification = np.int32(amplification)
     adata = sc.read_h5ad(file)
-    if (('x' not in adata.obs.keys()) | ('y' not in adata.obs.keys())) and 'spatial' in adata.obsm.keys():
-        position = adata.obsm['spatial']
-        x_min = position[:, 0].min() * amplification
-        y_min = position[:, 1].min() * amplification
-        adata.obs['x'] = merge_bin_coordinate(position[:, 0] * amplification, x_min, bin_size=bin_size)
-        adata.obs['y'] = merge_bin_coordinate(position[:, 1] * amplification, y_min, bin_size=bin_size)
-        adata.misc = {'up': position[:, 1].min(),
-                      'down': position[:, 1].max(),
-                      'left': position[:, 0].min(),
-                      'right': position[:, 0].max()}
+
+    position = adata.obsm['spatial']
+    x_min = position[:, 0].min() * amplification
+    y_min = position[:, 1].min() * amplification
+    adata.obs['x'] = merge_bin_coordinate(position[:, 0] * amplification, x_min, bin_size=bin_size)
+    adata.obs['y'] = merge_bin_coordinate(position[:, 1] * amplification, y_min, bin_size=bin_size)
+    adata.misc = {'up': position[:, 1].min(),
+                  'down': position[:, 1].max(),
+                  'left': position[:, 0].min(),
+                  'right': position[:, 0].max()}
     return adata
