ee7816c1751ad7f0336a5a94a7901d96c28f3058
Peisen Sun <sps@snnu.edu.cn>
2024-11-21 23:23:29 +0800

    Update AlgUtils.py

--- STMiner/Algorithm/AlgUtils.py
+++ STMiner/Algorithm/AlgUtils.py
@@ -8,18 +8,24 @@ def get_exp_array(adata, gene_name, remove_low_exp_spots=False):
 
 
 def _to_dense(remove_low_exp_spots, sparse_matrix):
-    dense_array = sparse_matrix.todense()
-    if remove_low_exp_spots:
-        dense_array = np.maximum(dense_array - np.mean(dense_array[dense_array != 0]), 0)
-    dense_array = np.array(np.round(dense_array), dtype=np.int32)
-    return dense_array
+    if sparse_matrix is not None:
+        dense_array = sparse_matrix.todense()
+        if remove_low_exp_spots:
+            dense_array = np.maximum(dense_array - np.mean(dense_array[dense_array != 0]), 0)
+        dense_array = np.array(np.round(dense_array), dtype=np.int32)
+        return dense_array
+    else:
+        print("matrix is None")
 
 
 def _preprocess(adata, gene_name):
-    exp_array = adata[:, adata.var_names == gene_name].X
-    if sparse.issparse(exp_array):
-        data = np.array(exp_array.todense())
+    if gene_name not in adata.var_names:
+        print(f"Warning: {gene_name} is not in adata gene list.")
     else:
-        data = np.array(exp_array)
-    sparse_matrix = sparse.coo_matrix((data[:, 0], (np.array(adata.obs['x']), np.array(adata.obs['y']))))
-    return sparse_matrix
+        exp_array = adata[:, adata.var_names == gene_name].X
+        if sparse.issparse(exp_array):
+            data = np.array(exp_array.todense())
+        else:
+            data = np.array(exp_array)
+        sparse_matrix = sparse.coo_matrix((data[:, 0], (np.array(adata.obs['x']), np.array(adata.obs['y']))))
+        return sparse_matrix
