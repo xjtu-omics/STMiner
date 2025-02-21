73d65ee1e71c3d8055eefc8980e72b4cd4d126bf
Peisen Sun <sps@snnu.edu.cn>
2025-02-21 14:58:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='STMiner',
-    version='0.0.3',
+    version='0.0.7',
     author='Peisen Sun',
     url='https://github.com/xjtu-omics/STMiner',
     license='MIT License',
