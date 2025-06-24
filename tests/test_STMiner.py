1dcf32d50534d7e2aafb0c2e0055b2afbeac0764
Peisen Sun <sunpeisen@stu.xjtu.edu.cn>
2025-06-24 16:14:01 +0800

    Update test_STMiner.py

--- tests/test_STMiner.py
+++ tests/test_STMiner.py
@@ -7,17 +7,21 @@ def test_spfinder():
     spfinder = SPFinder()
     assert spfinder.adata is None
 
+    # Test IO & merge_bin
     spfinder.read_h5ad("tests/data/test.h5ad", bin_size=5, merge_bin=True)
-    assert isinstance(spfinder.adata, ad.AnnData)
+    assert isinstance(spfinder.adata, ad.AnnData)    
     assert spfinder.adata.shape == (282, 32268)  #Test bin_size & merge_bin
 
+    # Test data preprocess
     spfinder.get_genes_csr_array(min_cells=200)
     assert len(spfinder.csr_dict) > 0
     assert np.mean(spfinder.csr_dict["rpl18a"].todense()) > 0
 
+    # Test SVG detection
     spfinder.spatial_high_variable_genes()
     assert len(spfinder.global_distance) > 0
 
+    # Test fitting
     spfinder.fit_pattern(
         n_comp=10, gene_list=list(spfinder.global_distance[:200]["Gene"])
     )
@@ -28,3 +32,5 @@ def test_spfinder():
 
     spfinder.cluster_gene(n_clusters=2, mds_components=20)
     assert len(spfinder.genes_labels) > 0
+
+
